5e362e269880f5f0b2726a9f30fbb8fcd56cf35c
BENABBI ANASS <lartiste7756@gmail.com>
2025-01-19 13:42:14 +0100

    second

---
+++ src/main/java/com/cigiproject/controller/FirstController.java
@@ -0,0 +1,5 @@
+package main.java.com.cigiproject.controller;
+
+public class FirstController {
+    
+}
---
+++ src/main/java/com/cigiproject/model/FirstModel.java
@@ -0,0 +1,5 @@
+package main.java.com.cigiproject.model;
+
+public class FirstModel {
+    
+}
